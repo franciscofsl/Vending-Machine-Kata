b3d12b9f66e00f07c2a072c590866cc3c788e98c
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-07-06 10:33:58 +0200

    add test to ensure vending machine accept nickels

---
+++ VendingMachine.Tests/VendingMachineTest.cs
@@ -0,0 +1,10 @@
+﻿namespace VendingMachine.Tests;
+
+public class VendingMachineTest
+{
+    [Fact]
+    public void Vending_Machine_Should_Accept_Nickels()
+    {
+        1.Should().Be(2);
+    }
+}
