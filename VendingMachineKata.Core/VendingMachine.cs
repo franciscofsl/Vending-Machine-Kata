ee2b8f49c79f2d4250d54d758e6afa7523f1bec5
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-07-06 11:37:53 +0200

    Show "INSERT COIN"

--- VendingMachineKata.Core/VendingMachine.cs
+++ VendingMachineKata.Core/VendingMachine.cs
@@ -7,6 +7,7 @@ public class VendingMachine
     }
 
     public double Amount { get; private set; }
+    public string Display { get; private set; } = "INSERT COIN";
 
     public static VendingMachine Initialize()
     {
