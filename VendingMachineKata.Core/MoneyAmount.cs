be02026720c8ba9ef7191df277d9008588cac968
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-07-07 12:12:02 +0200

    add money amount

---
+++ VendingMachineKata.Core/MoneyAmount.cs
@@ -0,0 +1,21 @@
+﻿namespace VendingMachineKata.Core;
+
+public class MoneyAmount
+{
+    private readonly double _value;
+
+    private MoneyAmount(double value)
+    {
+        _value = value;
+    }
+
+    public static MoneyAmount Of(double amount)
+    {
+        if (amount < 0)
+        {
+            throw new AggregateException("Money can´t have negative value.");
+        }
+
+        return new MoneyAmount(amount);
+    }
+}
---
+++ VendingMachineKata.Tests/MoneyAmountTest.cs
@@ -0,0 +1,15 @@
+﻿using VendingMachineKata.Core;
+
+namespace VendingMachineKata.Tests;
+
+public class MoneyAmountTest
+{
+    [Fact]
+    public void Should_Not_Create_Negative_Money_Amount()
+    {
+        FluentActions.Invoking(() => MoneyAmount.Of(-1))
+            .Should()
+            .ThrowExactly<AggregateException>()
+            .WithMessage("Money can´t have negative value.");
+    }
+}
