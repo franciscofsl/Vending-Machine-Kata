24d8a96592fa29734d9f33736858260ace449c21
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2024-07-12 19:33:50 +0200

    add return base type

---
+++ VendingMachineKata.Core/Return.cs
@@ -0,0 +1,16 @@
+﻿namespace VendingMachineKata.Core;
+
+public class Return
+{
+    private readonly List<Coin> _coins;
+
+    private Return(List<Coin> coins)
+    {
+        _coins = coins;
+    }
+
+    internal static Return Empty()
+    {
+        return new Return([]);
+    }
+}
